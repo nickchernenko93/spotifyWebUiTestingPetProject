4582fd05e40ff130aed361c48dab01001d60a990
Mykola Chernenko <mykola.chernenko@customertimes.com>
2023-10-12 12:43:36 +0300

    selenide properties added

--- src/main/java/com/spotify/pages/HomePage.java
+++ src/main/java/com/spotify/pages/HomePage.java
@@ -12,7 +12,7 @@ public class HomePage {
 
     private By homePageIcon = By.xpath("//span[contains(text(), 'Home')]");
 
-    public boolean isHomePageOpened(){
+    public boolean isHomePageOpened() {
         return $(homePageIcon).shouldBe(Condition.visible).isDisplayed();
     }
 }
