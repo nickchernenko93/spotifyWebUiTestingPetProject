8ce2323476457b74faaec21c676ec7652e27ef73
Mykola Chernenko <mykola.chernenko@customertimes.com>
2023-11-16 11:29:19 +0200

    Karate fix

--- src/test/java/com/spotify/api/KarateRunnerTest.java
+++ src/test/java/com/spotify/api/KarateRunnerTest.java
@@ -1,9 +1,9 @@
 package com.spotify.api;
 import com.intuit.karate.junit5.Karate;
 
-public class KarateTestRunner {
+public class KarateRunnerTest {
     @Karate.Test
     Karate getMyProfileDataAndVerifyUserName() {
-        return Karate.run("profileData").relativeTo(getClass());
+        return Karate.run("profiledata").relativeTo(getClass());
     }
 }
